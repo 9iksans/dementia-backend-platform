cde18edd19f9640d28c9b71a3bdb8085300a6af5
Luqmanul Hakim Iksan <luqmanhakimiksan@gmail.com>
2020-10-17 00:57:39 +0700

    mongo auth with monk

--- app/dbconnect.js
+++ app/dbconnect.js
@@ -1,5 +1,6 @@
+const mongo = require('mongodb')
 const monk = require('monk')
 
-const db = monk(process.env.MONGO_URI || "localhost/dementiaClassification")
+const db = monk("iksans:lulus3325@localhost:27017/dementiaClassification", {authSource: "admin"})
 
 module.exports = db
