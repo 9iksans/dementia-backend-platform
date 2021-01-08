3f5fde902cf196b155cd91e7ae5ce508e46a55f3
iksans <iksans@D-MacBookPro.local>
2021-01-08 21:51:22 +0700

    fixing some bugs on filesystem and add notification

--- pub.js
+++ pub.js
@@ -4,14 +4,19 @@ var client  = mqtt.connect('tcp://192.168.1.36:1883')
  
 client.on('connect', function(){
 
-    var pubs ={
-        "patient": "Timin Suprapti",
-        "urgent" : "To Much Sitting"
-    }
-    client.publish('/notification/lulus3355',JSON.stringify(pubs), {qos:1});
+    // var pubs ={
+    //     "patient": "Timin Suprapti",
+    //     "urgent" : "To Much Sitting"
+    // }
+    // client.publish('/notification/lulus3355',JSON.stringify(pubs), {qos:1});
 
-    // client.publish('/action/image',JSON.stringify(pubs), {qos:1});
-    //client.publish('/action/imrecog/5f8f2dd0c00456e8e03e5e9c',"Sitting", {qos:1});
+    var pubs ={
+        "image": "aaaa",
+        "imrecog" : "Standing"
+    }
+
+    client.publish('/action/image',JSON.stringify(pubs), {qos:1});
+    client.publish('/action/imrecog/5f8f2dd0c00456e8e03e5e9c',"Standing", {qos:1});
 
     console.log("success");
 
